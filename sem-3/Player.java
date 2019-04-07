124a5309c53e995dd83c765aae04b94c2987c4f3
Elena <helena557@mail.ru>
2019-04-07 15:17:53 +0300

    seminar 3

---
+++ sem-3/Player.java
@@ -0,0 +1,10 @@
+package ru.sbt.bit.ood.srp.sample1;
+
+public class Player {
+    private String firstname;
+    private String lastname;
+    private double speed;
+    private double accuracy;
+
+
+}
